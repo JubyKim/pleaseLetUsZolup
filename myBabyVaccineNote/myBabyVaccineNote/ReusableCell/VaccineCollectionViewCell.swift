27fd9d4a839c2fc6476df017ef822af71405f915
JUEUN KIM <jubykim@JUEUNui-MacBookPro.local>
2021-10-07 18:53:42 +0900

    homeView Button Set without layout

--- myBabyVaccineNote/myBabyVaccineNote/ReusableCell/VaccineCollectionViewCell.swift
+++ myBabyVaccineNote/myBabyVaccineNote/ReusableCell/VaccineCollectionViewCell.swift
@@ -11,14 +11,24 @@ import SnapKit
 
 class VaccineCollectionViewCell: UICollectionViewCell {
     let button = UIButton().then{
-        $0.setTitle("백신이름", for: .normal)
+        $0.layer.cornerRadius = 20
+        $0.clipsToBounds = true
+        $0.isUserInteractionEnabled = false
+    }
+    let buttonName = UILabel().then{
+        $0.textColor = .black
+        $0.isUserInteractionEnabled = false
     }
     
     func setupView() {
         self.addSubview(button)
-        
         button.snp.makeConstraints{
             $0.center.bottom.top.trailing.leading.equalToSuperview()
         }
+        button.addSubview(buttonName)
+        buttonName.snp.makeConstraints{
+            $0.leading.trailing.top.bottom.equalToSuperview()
+        }
+        
     }
 }
